9e48520048f1f1c0428f2513f1338a3c33704315
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2021-03-05 15:19:57 +0530

    Added Generic-main

---
+++ main/GenericMaxValueMain.java
@@ -0,0 +1,24 @@
+public class GenericMaxValue <E>{
+
+    E first, second,third;
+    public GenericMaxValue( E genericFirst, E genericSecond, E genericThird)
+    {
+        this.first=genericFirst;
+        this.second=genericSecond;
+        this.third=genericThird;
+    }
+    public <E extends Comparable<E>> E threeDifferentValues(E genericFirstValue, E genericSecondValue, E genericThirdValue)
+    {
+        E maxEValue = genericFirstValue;
+        if(genericSecondValue.compareTo(maxEValue) > 0)
+        {
+            genericFirstValue=genericSecondValue;
+        }
+
+        if(genericThirdValue.compareTo(maxEValue) > 0 )
+        {
+            genericFirstValue=genericThirdValue;
+        }
+        return genericFirstValue;
+    }
+}
